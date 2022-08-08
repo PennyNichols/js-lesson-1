929bc29d8f14b5faa77b77f05342aff0d338fd38
PennyNichols <106450742+PennyNichols@users.noreply.github.com>
2022-08-08 18:45:54 -0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,11 @@
+// console.time - start a timer
+
+console.time('timer1');
+var age = 12;
+console.log(age);
+
+// console.timeEnd - end a timer and print the time 
+console.timeEnd('timer1');
+
+console.error('This is an error');
+console.warn('This is a warning');
