97ad5c03afd7f29b84b3dbda4802d3629a6d6f82
pnichols789 <pnichols789@gmail.com>
2022-08-08 19:25:59 -0500

    -

---
+++ let_const.js
@@ -0,0 +1,13 @@
+// Declare the variable and assign the value together
+
+let name1 = 'John Doe';
+
+// Declare the variable
+
+let name2;
+
+name = 'John';
+name = 'Mike';
+
+// const pi = 3.14;  // const needs to have a value during declaration
+// pi = 3.15;      // can't overwrite
